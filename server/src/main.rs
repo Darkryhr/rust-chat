d7537315a9e42e2b9a25a6000ed8de7201304ad8
Gabriel Lellouche <gabyk800@gmail.com>
2022-08-31 16:48:37 +0300

    initial server setup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,47 @@
 use std::io::{ErrorKind, Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::sync::mpsc;
 use std::thread;
 
+//* constants
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
+    server
+        .set_nonblocking(true)
+        .expect("Failed to initialize non-blocking0");
+
+    let mut clients: Vec<TcpStream> = vec![];
+    let (tx, rx) = mpsc::channel::<String>();
+
+    loop {
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("client {} connected", addr);
+
+            let tx = tx.clone();
+            clients.push(socket.try_clone().expect("Failed to clone client"));
+
+            thread::spawn(move || loop {
+                let mut buff = vec![0; MSG_SIZE];
+
+                match socket.read_exact(&mut buff) {
+                    Ok(_) => {
+                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+
+                        println!("{}:{:?}", addr, msg);
+
+                        tx.send(msg).expect("Failed to send message");
+                    }
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("Closing connection with: {}", addr);
+                        break;
+                    }
+                }
+            });
+        }
+    }
 }
